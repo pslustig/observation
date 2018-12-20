ee6a58f0c2d256ff8d32643a8c65c3d792e2ef24
Peter Lustig <peter.lustig@physik.lmu.de>
2018-12-20 15:41:06 +0100

    added install_requires variable. Maybe change to find_packages() later?

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pslustig/reduce_telescope_data.git",
     packages=setuptools.find_packages(),
+    install_requires=['numpy', 'astropy', 'libtiff'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
