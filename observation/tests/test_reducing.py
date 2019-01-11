734184daeeeb66a2bfcfa122fc3e0f31ce32aab3
Peter Lustig <peter.lustig@physik.lmu.de>
2019-01-11 13:16:24 +0100

    added second test, trigger travis

--- observation/tests/test_reducing.py
+++ observation/tests/test_reducing.py
@@ -5,3 +5,8 @@ def test_band_from_filename():
     name = 'gfvvewiv-U.fits'
     band = obs.reducing.band_from_filename(name)
     assert band == 'U'
+
+
+def test_last_char_in_str():
+    name = '--'
+    assert obs.reducing.last_char_in_str(name, '-') == 1
